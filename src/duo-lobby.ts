425dad623db37af4aa91474d8aebd46813c6cb48
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-07-04 01:54:18 -0500

    Split the lobby with a p2p-switch

---
+++ src/duo-lobby.ts
@@ -0,0 +1,156 @@
+import { LitElement, html, customElement, property, css, internalProperty } from 'lit-element'
+import P2P, { Client } from '@mothepro/fancy-p2p'
+import type { RequestSelectedDetail } from '@material/mwc-list/mwc-list-item.js'
+import type { SafeListener } from 'fancy-emitter'
+
+import '@material/mwc-list'
+import '@material/mwc-list/mwc-list-item.js'
+import '@material/mwc-icon-button'
+import '@material/mwc-icon'
+
+export type NameChangeEvent = CustomEvent<string>
+
+declare global {
+  interface HTMLElementEventMap {
+    'p2p-error': ErrorEvent
+    'name-change': NameChangeEvent
+  }
+}
+
+@customElement('p2p-duo-lobby')
+export default class extends LitElement {
+  /** Name of the user. An anonymous one may be set be the server if left unassigned. */
+  @property({ type: String, reflect: true })
+  name = ''
+
+  /** Name of the user. An anonymous one may be set be the server if left unassigned. */
+  @property({ type: Boolean, reflect: true, attribute: 'can-change-name' })
+  canChangeName = false
+
+  /** Max length of user's name */
+  @property({ type: Number })
+  maxlength = 100
+
+  @property({ type: Object })
+  connection?: SafeListener<Client>
+
+  /** Others connected to the lobby. */
+  @internalProperty()
+  private clients: {
+    client: Client
+    action?: (accept: boolean) => void
+  }[] = []
+
+  @internalProperty()
+  private editing = false
+
+  static readonly styles = css`
+    :host([hidden]) {
+      display: none;
+    }
+    
+    :host mwc-icon-button {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+    
+    :host mwc-icon-button[part="reject"] {
+      margin-right: var(--mdc-icon-button-size, 48px);
+    }`
+
+  protected async updated(changed: Map<string | number | symbol, unknown>) {
+    if (changed.has('connection')) {
+      this.clients = []
+      for await (const client of this.connection!)
+        this.bindClient(client)
+    }
+  }
+
+  private bindClient = async (client: Client) => {
+    this.clients = [...this.clients, { client }]
+    for await (const { action, ack } of client.proposals) {
+      this.clients = this.clients.map(item =>
+        item.client == client
+          ? { client, action }
+          : item)
+
+      // Update UI every time a client accepts or rejects the proposal
+      ack.on(() => this.requestUpdate())
+        .catch(error => this.dispatchEvent(new ErrorEvent('p2p-error', { error })))
+        .finally(() => this.requestUpdate())
+    }
+    this.clients = this.clients.filter(({ client: currentClient }) => currentClient != client)
+  }
+
+  private nameChange(event: Event) {
+    event.preventDefault()
+    const detail = new FormData(event.target! as HTMLFormElement).get('name')?.toString() ?? ''
+    if (this.name != detail) {
+      this.name = detail
+      this.dispatchEvent(new CustomEvent('name-change', { detail }))
+    }
+    return this.editing = false
+  }
+
+  protected readonly render = () => html`
+    <mwc-list part="client-list" rootTabbable activatable>${this.clients.map(({ client, action }, index) =>
+    client.isYou
+      ? html`
+        <mwc-list-item
+          part="client is-you"
+          tabindex=${index}
+          hasMeta
+          @request-selected=${() => this.editing = true}>
+          ${this.editing && this.canChangeName
+          ? html`
+            <form @submit=${this.nameChange}>
+              <input
+                part="edit-name"
+                type="text"
+                autofocus 
+                name="name"
+                placeholder="Your name"
+                maxlength=${this.maxlength}
+                value=${client.name} />
+            </form>`
+          : client.name}
+          <mwc-icon part="edit-button" slot="meta">create</mwc-icon>
+        </mwc-list-item>`
+      : html`
+        <mwc-list-item
+          part="client is-other"
+          tabindex=${index}
+          ?hasMeta=${!action}
+          ?noninteractive=${!action && this.p2p!.groupExists(client)}
+          @request-selected=${({ detail: { selected } }: CustomEvent<RequestSelectedDetail>) => console.log(selected, !action, !this.p2p!.groupExists(client), selected && !action && !this.p2p!.groupExists(client) && this.p2p!.proposeGroup(client))}
+          >
+          ${client.name}
+          ${action
+          ? html`
+            <mwc-icon-button
+              part="accept"
+              icon="check_circle"
+              label="Aceept"
+              @click=${() => {
+              action(true)
+              this.clients = this.clients.map(item => item.client == client ? { client, action: undefined } : item)
+            }}></mwc-icon-button>
+            <mwc-icon-button
+              part="reject"
+              icon="cancel"
+              label="Reject"
+              @click=${() => {
+              action(false)
+              this.clients = this.clients.map(item => item.client == client ? { client, action: undefined } : item)
+            }}></mwc-icon-button>`
+          : this.p2p!.groupExists(client)
+            ? html`<mwc-icon part="waiting" slot="meta">hourglass_empty</mwc-icon>`
+            : html`<mwc-icon part="invite" slot="meta">add_circle</mwc-icon>`}
+        </mwc-list-item>`)}
+    </mwc-list>`
+}
+
+
+//@request-selected=${/** This is activated twice on rejection unforntuatley... */ }
+// @click=${ () => !action && !this.p2p!.groupExists(client) && this.p2p!.proposeGroup(client) }>
---
+++ src/switch.ts
@@ -0,0 +1,140 @@
+import storage from 'std:kv-storage'
+import { LitElement, html, customElement, property } from 'lit-element'
+import P2P, { State } from '@mothepro/fancy-p2p'
+import type { NameChangeEvent } from './duo-lobby.js'
+
+import './duo-lobby.js'
+
+/** Keys for storing data in kv-storage */
+const enum Keys {
+  /** The name of the user to connect in the lobby as. */
+  NAME = 'name'
+}
+
+declare global {
+  interface HTMLElementEventMap {
+    'p2p-error': ErrorEvent
+  }
+}
+
+@customElement('p2p-switch')
+export default class extends LitElement {
+  /** Name of the user. An anonymous one may be set be the server if left unassigned. */
+  @property({ type: String, reflect: true, noAccessor: true })
+  name = ''
+
+  /** List of STUN servers to broker P2P connections. */
+  @property({ type: Array })
+  stuns!: string[]
+
+  /** Address to the signaling server. */
+  @property({ type: String })
+  signaling!: string
+
+  /** Version of the signaling server. */
+  @property({ type: String, reflect: true })
+  version!: string
+
+  /** Number of times to attempt to make an RTC connection. Defaults to 1 */
+  @property({ type: Number, reflect: true })
+  retries!: number
+
+  @property({ type: String })
+  lobby!: string
+
+  /** The number of milliseconds to wait before giving up on the connection. Doesn't give up by default */
+  @property({ type: Number, reflect: true })
+  timeout!: number
+
+  /** Whether to store the user's name in local kv-storage. */
+  @property({ type: Boolean, attribute: 'local-storage' })
+  localStorage = false
+
+  /** Max length of user's name */
+  @property({ type: Number, attribute: 'maxlength' })
+  maxlength = 100
+
+  /** The minimum number of other connections that can be made in the lobby. */
+  @property({ type: Number, attribute: 'min-peers' })
+  minPeers = 1
+
+  /** The maximum number of other connections that can be made in the lobby. */
+  @property({ type: Number, attribute: 'max-peers' })
+  maxPeers = 10
+
+  private p2p?: P2P<ArrayBuffer>
+
+  // Connect once
+  protected async firstUpdated() {
+    this.connect(this.localStorage
+      ? (await storage.get(Keys.NAME) || '').toString()
+      : this.name)
+  }
+
+  private async connect(name: string) {
+    this.p2p?.leaveLobby()
+    this.p2p = new P2P(name, {
+      retries: this.retries,
+      timeout: this.timeout,
+      stuns: this.stuns,
+      lobby: this.lobby,
+      server: {
+        address: this.signaling,
+        version: this.version,
+      },
+    })
+
+    // Set my name on the attribute when it comes up
+    this.p2p.lobbyConnection.next.then(({ name }) => this.name = name)
+
+    try {
+      for await (const _ of this.p2p!.stateChange)
+        this.requestUpdate()
+    } catch (error) {
+      this.dispatchEvent(new ErrorEvent('p2p-error', { error }))
+    }
+    this.requestUpdate()
+  }
+
+  private nameChanged({ detail }: NameChangeEvent) {
+    if (this.localStorage)
+      storage.set(Keys.NAME, this.name = detail)
+    this.connect(this.name)
+  }
+
+  protected readonly render = () => {
+    if (this.p2p?.stateChange.isAlive)
+      switch (this.p2p!.state) {
+        case State.LOBBY:
+          return html`
+            <slot name="lobby"
+              .lobby-connection=${this.p2p.lobbyConnection}
+              ?can-change-name=${this.localStorage}
+              @name-change=${this.nameChanged}
+            >${this.minPeers == 1 && this.maxPeers == 1
+              ? html`
+              <p2p-duo-lobby
+                name=${this.name}
+                .lobby-connection=${this.p2p.lobbyConnection}
+              ></p2p-duo-lobby>`
+              : '???'
+            }</slot>`
+
+        case State.READY:
+          return html`
+            <slot
+              .broadcast=${this.p2p.broadcast}
+              .random=${this.p2p.random}
+              .peers=${this.p2p.peers}
+            >Ready</slot>`
+
+        case State.OFFLINE:
+          return html`<slot name="offline">Connecting</slot>`
+
+        case State.LOADING:
+          return html`<slot name="loading">Loading</slot>`
+      }
+
+    return html`<slot name="disconnected">Disconnected</slot>`
+  }
+}
