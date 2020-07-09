1e7770f9ae0dd90d3076135f80cb34d787c52b7d
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-07-09 17:51:20 -0500

    Allow generics on the p2p type forward

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import type P2P from '@mothepro/fancy-p2p'
+import type { default as P2P, Sendable } from '@mothepro/fancy-p2p'
 
 export { default } from './src/switch.js'
 export { default as duoLobby } from './src/duo-lobby.js'
 export { default as multiLobby } from './src/multi-lobby.js'
-export type peers = P2P['peers']
-export type broadcast = P2P['broadcast']
+export type peers<T extends Sendable = Sendable> = P2P<T>['peers']
+export type broadcast<T extends Sendable = Sendable> = P2P<T>['broadcast']
 export type random = P2P['random']
