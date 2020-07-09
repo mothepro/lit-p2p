804a34c1457d9031d0390c702c155701f96d163a
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-07-09 10:41:20 -0500

    Expose the types passed from fancy-p2p

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import type P2P from '@mothepro/fancy-p2p'
+
 export { default } from './src/switch.js'
 export { default as duoLobby } from './src/duo-lobby.js'
 export { default as multiLobby } from './src/multi-lobby.js'
+export type peers = P2P['peers']
+export type broadcast = P2P['broadcast']
+export type random = P2P['random']
